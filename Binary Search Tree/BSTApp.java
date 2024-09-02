8a9379d9d60db28581c0eea90657da99a9f97a23
Gabriela Lemos <124183874+GabrielaRBLemos@users.noreply.github.com>
2024-09-02 19:32:43 +0000

    Implementação de função para remover nó não recursivamente

--- Binary Search Tree/BSTApp.java
+++ Binary Search Tree/BSTApp.java
@@ -4,7 +4,7 @@ import BinarySearchTree.BST;
 
 public class BSTApp {
 	public static void main(String[] args){
-		BST tree = new BST();
+		BST<Integer> tree = new BST<Integer>();
 		int num, choice;
 		do {
 			Scanner scanner = new Scanner(System.in);
@@ -17,32 +17,37 @@ public class BSTApp {
 					tree.insert(num);
 					break;
 				case 2:
+					System.out.print("Digite o valor a ser removido:");
+					num = scanner.nextInt();
+					tree.remove(num);
+				break;
+				case 3:
 					System.out.print("Digite o valor a ser pesquisado:");
 					num = scanner.nextInt();
 					tree.contain(num);
 					break;
-				case 3:
+				case 4:
 					tree.min();
 					break;
-				case 4:
+				case 5:
 					tree.max();
 					break;
-				case 5:
+				case 6:
 					tree.byLevel();
 				break;
-				case 6:
+				case 7:
 					tree.inOrder();
 				break;
-				case 7:
+				case 8:
 					tree.preOrder();
 				break;
-				case 8:
+				case 9:
 					System.out.println("O número de nós é " + tree.numberOfNodes());
 				break;
-				case 9:
+				case 10:
 					System.out.println("O número de folhas é " + tree.numberOfLeafs());
 				break;
-				case 10:
+				case 11:
 					System.out.println("O número de nós não terminais é " + tree.numberOfNonTerminals());
 				break;
 				case 0:
@@ -56,8 +61,8 @@ public class BSTApp {
 		} while (choice != 0);
 	}
 	public static void printMenu(){
-		System.out.print("Opções:\n1 - Inserir valor\n2 - Confirmar presença de um valor\n3 - Exibir menor valor na árvore\n4 - Exibir maior valor na árvore\n5 - Exibir nós em ordem de nível\n" + //
-						"6- Exibir nós em ordem crescente\n7- Exibir nós verticalmente (pré-ordem)\n8- Exibir número de nós\n9- Exibir número de folhas\n \\n" + //
-						"10- Exibir número de nós não terminais\n0 - Encerrar programa\nInforme a opção: ");
+		System.out.print("Opções:\n1 - Inserir valor\n2 - Remover valor\n3 - Confirmar presença de um valor\n4 - Exibir menor valor na árvore\n5 - Exibir maior valor na árvore\n6 - Exibir nós em ordem de nível\n" + //
+						"7 - Exibir nós em ordem crescente\n8 - Exibir nós verticalmente (pré-ordem)\n9 - Exibir número de nós\n10 - Exibir número de folhas\n" + //
+						"11 - Exibir número de nós não terminais\n0 - Encerrar programa\nInforme a opção: ");
 	}
 }
