b7668a24465720e1c32cf27395c25cd6da338456
Gabriela Lemos <124183874+GabrielaRBLemos@users.noreply.github.com>
2024-09-13 18:11:02 +0000

    Created AVLTNode

---
+++ AVL Tree/AVLTree/AVLTNode.java
@@ -0,0 +1,43 @@
+package AVLTree;
+
+class AVLTNode<T extends Comparable<T>>{
+    private AVLTNode<T> left,right;
+    private T Value;
+    private int balFactor;
+
+    public AVLTNode(T value) {
+        Value = value;
+    }
+
+    public int getbalFactor() {
+        return balFactor;
+    }
+
+    public void setbalFactor(int balFactor) {
+        this.balFactor = balFactor;
+    }
+
+    public T getValue() {
+        return Value;
+    }
+    
+    public void setValue(T value) {
+        Value = value;
+    }
+
+    public AVLTNode<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(AVLTNode<T> left) {
+        this.left = left;
+    }
+
+    public AVLTNode<T> getRight() {
+        return right;
+    }
+
+    public void setRight(AVLTNode<T> right) {
+        this.right = right;
+    }
+}
